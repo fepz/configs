7d28b08dc4504d03db884ef74e53feb6e076a245
fepz <franpaez+github@gmail.com>
2020-05-26 09:33:52 -0300

    Custom.zsh file.

---
+++ zsh/custom.zsh
@@ -0,0 +1,10 @@
+# Personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. 
+#
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+alias sshso="ssh -A -p 28000 fp@if011.fi.mdn.unp.edu.ar"
+alias reload="exec zsh -l $SHELL"
